523d9d3eab9e1eb44925c971fd9966988b4a2e1b
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-10 03:30:47 +0300

    Script.lua

---
+++ Script.lua
@@ -0,0 +1,44 @@
+if game.PlaceId = 2753915549 then
+
+local Window = Rayfield:CreateWindow({
+   Name = "WrongHub",
+   LoadingTitle = "Loading...",
+   LoadingSubtitle = "by Wrongzy",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+local MainTab = Window:CreateWindow("AutoFarm", nil)
+local MainSection = Tab:CreateSection("Main")
+
+Rayfield:Notify({
+    Title = "Perfect !!",
+    Content = "Script Load!!",
+    Duration = 6.5,
+    Image = 4483362458,
+    Actions = { -- Notification Buttons
+       Ignore = {
+          Name = "Okay!",
+          Callback = function()
+          print("The user tapped Okay!")
+       end
+    },
+ },
+ })
