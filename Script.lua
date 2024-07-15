9c33a366d0891eef68fb14528419ff34101fe93a
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 22:40:39 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,19 +3,8 @@ local ArceusUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM
 ArceusUI:SetTitle("WrongHUB")
 
 local myButton = ArceusUI:AddButton("MyButton", function(...)
-    for L_116_forvar0, L_117_forvar1 in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
-			if L_117_forvar1:IsA("LocalScript") then
-				if L_117_forvar1.Name == "General" or L_117_forvar1.Name == "Shiftlock" or L_117_forvar1.Name == "FallDamage" or L_117_forvar1.Name == "4444" or L_117_forvar1.Name == "CamBob" or L_117_forvar1.Name == "JumpCD" or L_117_forvar1.Name == "Looking" or L_117_forvar1.Name == "Run" then
-					L_117_forvar1:Destroy()
-				end
-			end
-		end
-		for L_118_forvar0, L_119_forvar1 in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
-			if L_119_forvar1:IsA("LocalScript") then
-				if L_119_forvar1.Name == "RobloxMotor6DBugFix" or L_119_forvar1.Name == "Clans" or L_119_forvar1.Name == "Codes" or L_119_forvar1.Name == "CustomForceField" or L_119_forvar1.Name == "MenuBloodSp" or L_119_forvar1.Name == "PlayerList" then
-					L_119_forvar1:Destroy()
-				end
-			end
-		end
-	end
+First_Sea = false
+local L_83_ = game.PlaceId
+if L_83_ == 2753915549 then
+	First_Sea = true
 end, myExtraValues)
