54781bceab29b54eefc75413b39861848b1e7411
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 22:18:59 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,87 +1,27 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Michisad Hub",
-   LoadingTitle = "Loading",
-   LoadingSubtitle = "Access granted. Script made by Michisad",
+   Name = "Rayfield Example Window",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
    ConfigurationSaving = {
-      Enabled = false,
+      Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "ISuckAtCoding"
+      FileName = "Big Hub"
    },
    Discord = {
       Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      Invite = "sirius", -- The Discord invite code, do not include discord.gg/
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
-      Title = "Untitled",
+      Title = "Sirius Hub",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      Note = "Join the discord (discord.gg/sirius)",
+      FileName = "SiriusKey",
+      SaveKey = true,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Key = "Hello"
    }
-})
-
-local MainTab = Window:CreateTab("Home", nil) -- Title, Image
-local MainSection = MainTab:CreateSection("Main")
-
-Rayfield:Notify({
-   Title = "Thanks for using Michisad's HUB",
-   Content = "best GUI ever in the world!!!",
-   Duration = 5,
-   Image = nil,
-   Actions = { -- Notification Buttons
-      Ignore = {
-         Name = "Yes!!!", "ur wrong :(",
-         Callback = function()
-         print("The user tapped Yes!!!")
-      end
-   },
-},
-})
-
-local Button = MainTab:CreateButton({
-   Name = "",
-   Callback = function()
-               --Toggles the infinite jump between on or off on every script run
-_G.infinjump = not _G.infinjump
-
-if _G.infinJumpStarted == nil then
-    --Ensures this only runs once to save resources
-    _G.infinJumpStarted = true
-
-    --Notifies readiness
-    game.StarterGui.SetCore("SendNotification", {Title="Youtube Hub"; Text="Infinite Jump Activated!"; Duration=5;})
-
-    --The actual infinite jump
-    local plr = game:GetService('Players').LocalPlayer
-    local m = plr:GetMouse()
-    m.KeyDown:connect(function(k)
-        if _G.infinjump then
-            if k:byte() == 32 then
-            humanoid = game:GetService'Players'.LocalPlayer.Character:FindFirstChildOfClass('Humanoid')
-            humanoid:ChangeState('Jumping')
-            wait()
-            humanoid:ChangeState('Seated')
-            end
-        end
-    end
-end
-   end,
-})
-
-    local Slider = MainTab:CreateSlider({
-   Name = "WalkSpeed",
-   Range = {0, 250},
-   Increment = 5,
-   Suffix = "Speed",
-   CurrentValue = 16,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (value)
-   end,
 })
