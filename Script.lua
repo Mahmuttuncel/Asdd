1069869d1efcae87a0bf9df09d0172fbf6f1d6a6
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 22:38:09 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,55 +1,21 @@
-local library = loadstring(game:HttpGet("https://github.com/GoHamza/AppleLibrary/blob/main/main.lua?raw=true"))()
+local ArceusUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/ArceusX-V3-Scripts/main/ArceusUI.lua"))()
 
+ArceusUI:SetTitle("WrongHUB")
 
-local window = library:init("Titlebar", true, Enum.KeyCode.RightShift, true)
-
-window:Divider("I'm a divider!")
-
-local sectionA = window:Section("Test Elements")
-
-sectionA:Divider("I'm another divider!")
-
-sectionA:Button("Click me!", function()
-   print("Button clicked.")
-end)
-
-sectionA:Label("Lorem ipsum dolor sit amet.")
-
-sectionA:Switch("Switch me!", false, function(a)
-   print(a)
-end)
-
-sectionA:TextField("Enter text here!", "Enter text here...", function(a)
-   print(a)
-end)
-
-window:Divider("Just dividin'")
-
-local sectionB = window:Section("Test Notifications")
-
-sectionB:Divider("Dividers are cool!")
-
-sectionB:Button("Temporary Notification", function()
-   window:TempNotify("Be careful!", "We are going to beat you up.", "rbxassetid://12608259004")
-end)
-
-sectionB:Button("Notification 1", function() window:Notify("Hello!", "I am notification", "Button1", "rbxassetid://12608259004",
-   function()
-       print(1)
-   end)
-end)
-
-sectionB:Button("Notification 2", function() window:Notify2("Hello!", "I am notification", "Button 1", "Button 2", "rbxassetid://12608259004",
-   function()
-       print(1)
-   end,
-   function()
-       print(2)
-   end)
-end)
-
-window:GreenButton(function()
-   print("You clicked the green button!")
-end)
-
-sectionA:Select()
+local myButton = ArceusUI:AddButton("MyButton", function(...)
+    for L_116_forvar0, L_117_forvar1 in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
+			if L_117_forvar1:IsA("LocalScript") then
+				if L_117_forvar1.Name == "General" or L_117_forvar1.Name == "Shiftlock" or L_117_forvar1.Name == "FallDamage" or L_117_forvar1.Name == "4444" or L_117_forvar1.Name == "CamBob" or L_117_forvar1.Name == "JumpCD" or L_117_forvar1.Name == "Looking" or L_117_forvar1.Name == "Run" then
+					L_117_forvar1:Destroy()
+				end
+			end
+		end
+		for L_118_forvar0, L_119_forvar1 in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
+			if L_119_forvar1:IsA("LocalScript") then
+				if L_119_forvar1.Name == "RobloxMotor6DBugFix" or L_119_forvar1.Name == "Clans" or L_119_forvar1.Name == "Codes" or L_119_forvar1.Name == "CustomForceField" or L_119_forvar1.Name == "MenuBloodSp" or L_119_forvar1.Name == "PlayerList" then
+					L_119_forvar1:Destroy()
+				end
+			end
+		end
+	end
+end, myExtraValues)
