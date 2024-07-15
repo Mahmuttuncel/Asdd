39ddc3810f14746bc1d1ddcbb61adbc00175c085
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 22:41:38 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,6 +1,7 @@
 local ArceusUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/SPDM-Team/ArceusX-V3-Scripts/main/ArceusUI.lua"))()
 
 ArceusUI:SetTitle("WrongHUB")
+ArceusUI:SetLogo("MyLogo.png", "https://mywebsite/myimage.png")
 
 local myButton = ArceusUI:AddButton("MyButton", function(...)
     local extraValues = {...}
