169274047656167fd480f8fd4a2969f486667e5e
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 21:00:34 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,20 +1,59 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Main = Window:MakeTab({
+local Window = OrionLib:MakeWindow({Name = "WrongHUB / Demon Blade", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Misc = Window:MakeTab({
 	Name = "Main",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 OrionLib:MakeNotification({
-	Name = "AutoFarm",
-	Content = "AutoFarm is Here",
+	Name = "Script Loading...",
+	Content = "WrongHub is Loading...",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
-Main:AddButton({
-	Name = "Button!",
+Misc:AddButton({
+	Name = "FpsBoost",
 	Callback = function()
-      		print("button pressed")
+      if _G.FPSBoost then
+            wait(3)
+            local decalsyeeted = true
+            local g = game
+            local w = g.Workspace
+            local l = g.Lighting
+            local t = w.Terrain
+            t.WaterWaveSize = 0
+            t.WaterWaveSpeed = 0
+            t.WaterReflectance = 0
+            t.WaterTransparency = 0
+            l.GlobalShadows = false
+            l.FogEnd = 9e9
+            l.Brightness = 0
+            settings().Rendering.QualityLevel = "Level01"
+            for i, v in pairs(g:GetDescendants()) do
+               if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
+                  v.Material = "Plastic"
+                  v.Reflectance = 0
+               elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+                  v.Transparency = 1
+               elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+                  v.Lifetime = NumberRange.new(0)
+               elseif v:IsA("Explosion") then
+                  v.BlastPressure = 1
+                  v.BlastRadius = 1
+               elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+                  v.Enabled = false
+               elseif v:IsA("MeshPart") then
+                  v.Material = "Plastic"
+                  v.Reflectance = 0
+                  v.TextureID = 10385902758728957
+               end
+            end
+            for i, e in pairs(l:GetChildren()) do
+               if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+                  e.Enabled = false
+               end
+            end
+         end
   	end    
 })
 Tab:AddToggle({
