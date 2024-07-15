73bac8f31fcd57cf4d1460573f8372427556e44e
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 22:04:10 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,7 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local tps = game:GetService("TeleportService")
-local placeid = 2753915549 -- change it to your game id
 local Tab = Window:MakeTab({
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
