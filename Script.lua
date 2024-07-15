186548a84febe7c1a594cc413faa77b327c48a89
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 21:30:41 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,14 @@
+	local L_82_ = game.PlaceId
+	if L_82_ == 2753915549 then
+		First_Sea = true;
+	elseif L_82_ == 4442272183 then
+		Second_Sea = true;
+	elseif L_82_ == 7449423635 then
+		Third_Sea = true;
+	else
+		game:Shutdown()
+	end;
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "WrongHUB / Demon Blade", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 local Misc = Window:MakeTab({
@@ -12,50 +23,11 @@ OrionLib:MakeNotification({
 	Time = 5
 })
 Misc:AddButton({
-	Name = "FpsBoost",
+	Name = "FirstSEA",
 	Callback = function()
-if _G.FPSBoost then
-        spawn(function()
-		wait(3)
-		local decalsyeeted = true
-		local g = game
-		local w = g.Workspace
-		local l = g.Lighting
-		local t = w.Terrain
-		t.WaterWaveSize = 0
-		t.WaterWaveSpeed = 0
-		t.WaterReflectance = 0
-		t.WaterTransparency = 0
-		l.GlobalShadows = false
-		l.FogEnd = 9e9
-		l.Brightness = 0
-		settings().Rendering.QualityLevel = "Level01"
-		for i, v in pairs(g:GetDescendants()) do
-			if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then 
-				v.Material = "Plastic"
-				v.Reflectance = 0
-			elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
-				v.Transparency = 1
-			elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
-				v.Lifetime = NumberRange.new(0)
-			elseif v:IsA("Explosion") then
-				v.BlastPressure = 1
-				v.BlastRadius = 1
-			elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
-				v.Enabled = false
-			elseif v:IsA("MeshPart") then
-				v.Material = "Plastic"
-				v.Reflectance = 0
-				v.TextureID = 10385902758728957
-			end
-		end
-		for i, e in pairs(l:GetChildren()) do
-			if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
-				e.Enabled = false
-			end
-		end
-	end
-end 
+        local L_83_ = game.PlaceId
+	if L_83_ == 2753915549
+end
 })
 Tab:AddToggle({
 	Name = "This is a toggle!",
