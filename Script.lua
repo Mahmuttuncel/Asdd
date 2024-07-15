5437b87a0460a56acaebdfc0b98e1802193d3015
Mahmuttuncel <85134147+Mahmuttuncel@users.noreply.github.com>
2024-07-15 21:37:19 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,15 +1,21 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "WrongHUB / Demon Blade", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Misc = Window:MakeTab({
-	Name = "Main",
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 OrionLib:MakeNotification({
-	Name = "Script Loading...",
-	Content = "WrongHub is Loading...",
+	Name = "Title!",
+	Content = "Notification content... what will it say??",
 	Image = "rbxassetid://4483345998",
 	Time = 5
+})
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
 })
 Tab:AddToggle({
 	Name = "This is a toggle!",
